6baa116ec388132e4dc6cd5b6bd3c015c260764b
Shourya Sharma <shouryano01@gmail.com>
2026-07-02 19:54:40 +0530

    first commit

---
+++ src/main/java/com/shourya/springbootdemo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.shourya.springbootdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @GetMapping("/")
+    public String hello(){
+        return "hello shourya";
+    }
+}
